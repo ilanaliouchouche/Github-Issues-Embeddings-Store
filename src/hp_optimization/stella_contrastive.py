f607fcea605ad67a456e07061f3c11ef2af053ec
Ilan Aliouchouche <ialiouch@margaret.saclay.inria.fr>
2025-02-07 00:06:31 +0100

    training launched

--- src/hp_optimization/stella_contrastive.py
+++ src/hp_optimization/stella_contrastive.py
@@ -3,12 +3,16 @@ from datasets import load_dataset
 from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, losses, SentenceTransformerTrainingArguments
 from sentence_transformers.evaluation import BinaryClassificationEvaluator
 import multiprocessing
+import warnings
+
+warnings.filterwarnings("ignore")
 
 SEED = 42
 
-num_workers = min(multiprocessing.cpu_count()//2, 4)
+num_workers = multiprocessing.cpu_count()
 
 dataset_dict = load_dataset("WhereIsAI/github-issue-similarity", "default")
+print(dataset_dict)
 dataset_dict = dataset_dict.rename_columns({"text1": "sentence1", "text2": "sentence2"})
 
 binary_acc_evaluator = BinaryClassificationEvaluator(
@@ -19,8 +23,8 @@ binary_acc_evaluator = BinaryClassificationEvaluator(
 )
 
 def remove_html_tags(sample):
-    sample["sentence1"] = BeautifulSoup(sample["text1"], "html.parser").get_text()
-    sample["sentence2"] = BeautifulSoup(sample["text2"], "html.parser").get_text()
+    sample["sentence1"] = BeautifulSoup(sample["sentence1"], "html.parser").get_text()
+    sample["sentence2"] = BeautifulSoup(sample["sentence2"], "html.parser").get_text()
     return sample
 
 dataset_dict = dataset_dict.map(remove_html_tags, num_proc=num_workers)
@@ -28,7 +32,7 @@ dataset_dict = dataset_dict.map(remove_html_tags, num_proc=num_workers)
 def hpo_search_space(trial):
     return {
         "num_train_epochs": trial.suggest_int("num_train_epochs", 1, 8),
-        "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", [32, 64, 128, 160]),
+        "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", [8, 16, 32]),
         "learning_rate": trial.suggest_float("learning_rate", 1e-6, 1e-4, log=True),
         "warmup_ratio": trial.suggest_float("warmup_ratio", 0.05, 0.3),
         "weight_decay": trial.suggest_float("weight_decay", 0.0, 0.3),
@@ -37,7 +41,8 @@ def hpo_search_space(trial):
     }
 
 def hpo_model_init(trial):
-    return SentenceTransformer("NovaSearch/stella_en_400M_v5")
+    return SentenceTransformer("NovaSearch/stella_en_400M_v5",
+                               trust_remote_code=True)
 
 def hpo_loss_init(model):
     return losses.ContrastiveLoss(model)
@@ -47,14 +52,15 @@ def hpo_compute_objective(metrics):
 
 args = SentenceTransformerTrainingArguments(
     output_dir=f"checkpoints/stella_contrastive",
-    fp16=True,
+    # fp16=True,
     bf16=True,
     eval_strategy="no",
     save_strategy="no",
     logging_steps=10,
     run_name="stella-contrastive-hpo",
     seed=SEED,
-    report_to=["tensorboard", "wandb"]
+    logging_dir="./runs/hpo-logs",
+    report_to=["tensorboard"]
 )
 
 trainer = SentenceTransformerTrainer(
@@ -64,8 +70,7 @@ trainer = SentenceTransformerTrainer(
     eval_dataset=dataset_dict["valid"],
     evaluator=binary_acc_evaluator,
     model_init=hpo_model_init,
-    loss=hpo_loss_init,
-    logging_dir="./runs/hpo-logs"
+    loss=hpo_loss_init
 )
 
 best_trial = trainer.hyperparameter_search(
